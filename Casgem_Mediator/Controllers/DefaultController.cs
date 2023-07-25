5b14bb1eb9ac97b9cabe3ea8f5724a9b9cda90f8
MuratYucedag <yucedagmurat23@gmail.com>
2023-07-25 15:53:40 +0300

    Add project files.

---
+++ Casgem_Mediator/Controllers/DefaultController.cs
@@ -0,0 +1,21 @@
+﻿using Casgem_Mediator.MediatorPattern.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Casgem_Mediator.Controllers
+{
+    public class DefaultController : Controller
+    {
+        private readonly IMediator _meditor;
+        public DefaultController(IMediator meditor)
+        {
+            _meditor = meditor;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var values =await _meditor.Send(new GetProductQuery());
+            return View(values);
+        }
+    }
+}
